ed675970183836c4d3d5aa32a93958dbf24e9340
amir bidva <a.bidva@gmail.com>
2018-01-30 00:18:05 -0500

    BinaryTree

---
+++ algorithms/BinaryTree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num;
+            string unsortedArray = "";
+            string sortedArray = "";
+            num = rnd.Next(0, 100);
+            unsortedArray = unsortedArray + num.ToString().PadLeft(3);
+            Tree myTree = new Tree(num);
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                num = rnd.Next(0, 100);
+                unsortedArray = unsortedArray + num.ToString().PadLeft(3);
+                myTree.AddRc(num);
+            }
+            string treeString = "";
+            myTree.Print(null, ref treeString);
+            Console.WriteLine(treeString);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ algorithms/BinaryTree/Tree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Tree
+    {
+        Node top;
+        public Tree()
+        {
+            top = null;
+        }
+        public Tree(int i)
+        {
+            top = new Node(i);
+        }
+        public void Add(int value)
+        {
+            if (top == null)
+            {
+                Node newNode = new Node(value);
+                top = newNode;
+                return;
+            }
+            Node currentNode = top;
+            bool added = false;
+            do
+            {
+                if (value < currentNode.value)
+                {
+                    if (currentNode.left == null)
+                    {
+                        Node NewNode = new Node(value);
+                        currentNode.left = NewNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.left;
+                    }
+                }
+                else
+                {
+                    if (currentNode.right == null)
+                    {
+                        Node NewNode = new Node(value);
+                        currentNode.right = NewNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+
+            } while (!added);
+
+        }
+        public void AddRc(int value)
+        {
+            AddR(ref top, value);
+        }
+        private void AddR(ref Node N, int value)
+        {
+            if (N == null)
+            {
+                Node NewNode = new Node(value);
+                N = NewNode;
+                return;
+            }
+            if (value < N.value)
+            {
+                AddR(ref N.left, value);
+                return;
+            }
+            else
+            {
+                AddR(ref N.right, value);
+                return;
+            }
+        }
+        public void Print(Node N, ref string s)
+        {
+            if (N == null)
+            {
+                N = top;
+
+            }
+            if (N.left != null)
+            {
+                Print(N.left, ref s);
+                s = s + N.value.ToString().PadLeft(3);
+            }
+            else
+            {
+                s = s + N.value.ToString().PadLeft(3);
+            }
+            if (N.right != null)
+            {
+                Print(N.right, ref s);
+            }
+        }
+    }
+}

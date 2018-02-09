97d9c04364d50aa75c3b758dcdedc2cd8f711b33
amir bidva <a.bidva@gmail.com>
2018-02-09 18:18:18 -0500

    swap minmax and selection sort

---
+++ algorithms/SwapMinMaxArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapMinMaxArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 15, 22, 5, 7, 4, 12, 44, 11, 16, 2 };
+            int[] res = SwapMinMaxArray(arr);
+        }
+
+        private static int[] SwapMinMaxArray(int[] arr)
+        {
+            int min = 0;
+            int max = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[min] > arr[i])
+                    min = i;
+                if (arr[max] < arr[i])
+                    max = i;
+            }
+            int temp = arr[min];
+            arr[min] = arr[max];
+            arr[max] = temp;
+            return arr;
+        }
+    }
+}
---
+++ algorithms/selection-sort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selection_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 3,9,4,2,6 };
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int min_pos = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min_pos])
+                    {
+                        min_pos = j;
+                    }
+                }
+                int tmp = arr[i];
+                arr[i] = arr[min_pos];
+                arr[min_pos] = tmp;
+            }
+        }
+    }
+}
